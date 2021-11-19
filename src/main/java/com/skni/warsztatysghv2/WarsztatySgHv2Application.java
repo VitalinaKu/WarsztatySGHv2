3b3d41927c2646e4063ce776f695020a08171f4e
VitalinaKu <vitalina.photo@gmail.com>
2021-11-19 21:59:28 +0100

    Dependency injection - field

--- src/main/java/com/skni/warsztatysghv2/WarsztatySgHv2Application.java
+++ src/main/java/com/skni/warsztatysghv2/WarsztatySgHv2Application.java
@@ -1,13 +1,19 @@
 package com.skni.warsztatysghv2;
 
 import com.skni.warsztatysghv2.registration.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
 
+@Component
 @SpringBootApplication
 public class WarsztatySgHv2Application {
+
+    @Autowired
+    private StudentService studentService;
 
     public static void main(String[] args) {
         SpringApplication.run(WarsztatySgHv2Application.class, args);
@@ -15,7 +21,7 @@ public class WarsztatySgHv2Application {
 
     @EventListener(ApplicationReadyEvent.class)
     public void doAfterStartup() {
-        new StudentService().printStudent();
+        studentService.printStudent();
     }
 
 }
