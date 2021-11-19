44e93f8bbcfba4a460450a004aca28d9a666a21d
VitalinaKu <vitalina.photo@gmail.com>
2021-11-19 21:21:06 +0100

    Dependency injection - constructor

--- src/main/java/com/skni/warsztatysghv2/WarsztatySgHv2Application.java
+++ src/main/java/com/skni/warsztatysghv2/WarsztatySgHv2Application.java
@@ -1,13 +1,22 @@
 package com.skni.warsztatysghv2;
 
 import com.skni.warsztatysghv2.registration.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
 
+@Component
 @SpringBootApplication
 public class WarsztatySgHv2Application {
+    private final StudentService studentService;
+
+    @Autowired
+    public WarsztatySgHv2Application(StudentService studentService) {
+        this.studentService = studentService;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(WarsztatySgHv2Application.class, args);
@@ -15,7 +24,7 @@ public class WarsztatySgHv2Application {
 
     @EventListener(ApplicationReadyEvent.class)
     public void doAfterStartup() {
-        new StudentService().printStudent();
+        studentService.printStudent();
     }
 
 }
